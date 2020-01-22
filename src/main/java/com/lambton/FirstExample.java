73ab3edff08f6fe0e86baee30e48e295b0ebe60a
Simranjot singh <simusimsim@gmail.com>
2020-01-21 21:03:06 -0500

    Commit for Day 1

--- src/main/java/com/lambton/FirstExample.java
+++ src/main/java/com/lambton/FirstExample.java
@@ -7,6 +7,10 @@ public class FirstExample {
         System.out.println(" Welcome to Java ");
 
         //Sample commit ~ 1
+
+        for (int i=1;i<=10;i++){
+            System.out.println("Simranjot");
+        }
     }
 
 
